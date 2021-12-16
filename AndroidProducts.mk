1d9a879ded2fa45476ecf1098c43b38b5a013826
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-16 13:23:46 +0100

    rmx3370: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: realme Model: realme gt neo2 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rmx3370.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rmx3370-user \
+    omni_rmx3370-userdebug \
+    omni_rmx3370-eng
